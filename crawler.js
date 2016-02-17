beff633134c41004af8be01ed243b3e6bfde42e6
calvinvbigyi <czhou2@sysomos.com>
2016-02-17 12:45:35 -0800

    first commit for the crawler

---
+++ crawler.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+var fs = require('fs');
+
+var pageToVisit = "https://ugradcalendar.uwaterloo.ca/page/ENG-Computer-Engineering-Electrical-Engineering";
+console.log("Visiting page " + pageToVisit);
+request(pageToVisit, function(error, response, body) {
+   if(error) {
+     console.log("Error: " + error);
+   }
+   // Check status code (200 is HTTP OK)
+   console.log("Status code: " + response.statusCode);
+   if(response.statusCode === 200) {
+     // Parse the document body
+     var $ = cheerio.load(body);
+     $('td:has(a)').each(function(index) {
+     	var courseName = $(this).text().trim();
+    	var link = $(this).attr('href');
+    	fs.appendFileSync('ECECourseRequirement.txt', courseName + '\n' + link + '\n');
+     });
+   }
+});
+
+var bioEngPage = "https://ugradcalendar.uwaterloo.ca/page/ENG-Biomedical-Engineering";
