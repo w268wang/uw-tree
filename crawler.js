6d21d3f977678aabf5d5fcee349bf974575d50be
calvinvbigyi <czhou2@sysomos.com>
2016-03-12 16:38:23 -0800

    all engineering major raw

--- crawler.js
+++ crawler.js
@@ -17,7 +17,10 @@ request(pageToVisit, function(error, response, body) {
      $('td:has(a)').each(function(index) {
      	var courseName = $(this).text().trim();
     	var link = $(this).attr('href');
-    	fs.appendFileSync('ECE.txt', courseName + ' ' + link + '\n');
+    	var file = fs.readFileSync('ECE.txt', {encoding: 'utf-8'});
+    	if (file.size === 0) {
+    		fs.appendFileSync('ECE.txt', courseName + ' ' + link + '\n');
+    	}
      });
    }
 });
@@ -36,7 +39,36 @@ request(bioEngPage, function(error, response, body) {
 	    $('tr').each(function(index) {
 	     	var courseName = $(this).text().trim();
 	    	var link = $(this).attr('href');
-	    	fs.appendFileSync('BioEng.txt', courseName + ' ' + link + '\n');
+	    	var file = fs.readFileSync('BioEng.txt', {encoding: 'utf-8'});
+    		if (file.size === 0) {
+    			fs.appendFileSync('BioEng.txt', courseName + ' ' + link + '\n');
+    		}
 	    });
    	}
 });
+
+var engineeringMajorPage = [
+	"http://ugradcalendar.uwaterloo.ca/page/ENG-Civil-Engineering",
+	"http://ugradcalendar.uwaterloo.ca/page/ENG-Environmental-Engineering",
+	"http://ugradcalendar.uwaterloo.ca/page/ENG-Geological-Engineering"
+];
+
+  var str = "http://ugradcalendar.uwaterloo.ca/page/ENG-Mechanical-Engineering";
+  request(str, function(error, response, body) {
+    if(error) {
+       console.log("Error: " + error);
+      }
+      // Check status code (200 is HTTP OK)
+      console.log("Status code: " + response.statusCode);
+      if(response.statusCode === 200) {
+        // Parse the document body
+        var $ = cheerio.load(body);
+        $('td').each(function(index) {
+          var courseName = $(this).text().trim();
+          var link = $(this).attr('href');
+          fs.appendFileSync('/Users/calvinzhou/devel/uw-tree/engineering/ENG-Mechanical-Engineering.txt', courseName + ' ' + link + '\n');
+        });
+      }
+  });
+
+
